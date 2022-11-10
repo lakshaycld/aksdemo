4c0b20664f867336908fdd86b44608714a444a9e
lakshaycld <115004934+lakshaycld@users.noreply.github.com>
2022-11-11 00:20:18 +0530

    Update provider.tf

--- demoaks/provider.tf
+++ demoaks/provider.tf
@@ -1,5 +1,5 @@
-provider "azurerm"{
-  features{}
+provider "azurerm" {
+  features {}
 
   version = "=3.0.0"
 
@@ -7,9 +7,9 @@ provider "azurerm"{
 
 terraform {
  backend "azurerm" {
-    storage_account_name ="aksstoragelak1"
-    container_name       ="lakcontainer"
-    key                  ="prod.lakshay.tfstate"
-    access key           ="4k4YASLQLFSdHzlTxsqsLrfBx7DmEK4BOXt7Q0bYsH8vm+H7kxvCg6MGkXg6N9b8e6kIl/E9KfZI+AStd7XoWA=="
+    storage_account_name = "aksstoragelak1"
+    container_name       = "lakcontainer"
+    key                  = "prod.lakshay.tfstate"
+    access key           = "4k4YASLQLFSdHzlTxsqsLrfBx7DmEK4BOXt7Q0bYsH8vm+H7kxvCg6MGkXg6N9b8e6kIl/E9KfZI+AStd7XoWA=="
   }
 }
